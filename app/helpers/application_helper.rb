bfe688a333d671c9599bcf0f19864477b7f7ab4d
Khoadeptrai <96755096+dkdeptrai@users.noreply.github.com>
2024-09-30 22:46:31 +0700

    feat: extracted vote buttons to shared prefenrences with updated styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def vote_paths(votable)
+    if votable.is_a?(Post)
+      {
+        upvote_path: upvote_post_path(votable, post_id: votable.id),
+        downvote_path: downvote_post_path(votable, post_id: votable.id),
+        turbo_frame: dom_id(votable)
+      }
+    elsif votable.is_a?(Comment)
+      commentable = votable.commentable
+      {
+        upvote_path: upvote_post_comment_path(votable.commentable, votable, comment_id: votable.id),
+        downvote_path: downvote_post_comment_path(votable.commentable, votable, comment_id: votable.id),
+        turbo_frame: dom_id(votable)
+      }
+    end
+  end
 end
